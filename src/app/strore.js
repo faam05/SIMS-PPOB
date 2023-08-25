ea39fdbab4b0ef740ba7dcf6d59afc3ba237b656
faam05 <fadilfadoli30@gmail.com>
2023-08-25 08:36:10 +0700

    fix: responsive design and disable reduxdev

--- src/app/strore.js
+++ src/app/strore.js
@@ -11,7 +11,7 @@ const store = configureStore({
         services: servicesReducer,
         banners: bannerReducer,
     },
-    // devTools: false,
+    devTools: false,
 });
 
 export default store;
